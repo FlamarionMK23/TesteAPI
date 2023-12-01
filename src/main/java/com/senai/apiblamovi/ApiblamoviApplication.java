93a1736ad70e2167f7411662dc929ca56dd61dee
Flamarion Souza <flamarionsouza23@gmail.com>
2023-12-01 20:40:33 -0300

    teste

--- src/main/java/com/senai/apiblamovi/ApiblamoviApplication.java
+++ src/main/java/com/senai/apiblamovi/ApiblamoviApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiblamoviApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiblamoviApplication.class, args);
 	}
